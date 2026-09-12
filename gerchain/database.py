e3cd1ad4dc87b4d21f72231e8c55ad0290b8ea43
butnee-sys <butnee@gmail.com>
2026-09-13 03:10:18 +0800

    feat: make GerChain database URL configurable for sandbox

--- gerchain/database.py
+++ gerchain/database.py
@@ -1,8 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./gerchain.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "GERCHAIN_DATABASE_URL",
+    "sqlite:///./gerchain.db",
+)
+
+connect_args = {}
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args=connect_args,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
