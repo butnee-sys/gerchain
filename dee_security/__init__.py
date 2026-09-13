18836bba7683c460fc7beae363fad97d5c2aad06
butnee-sys <butnee@gmail.com>
2026-09-13 22:38:02 +0800

    security: expose DEE release signing primitives

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -8,14 +8,19 @@ from .root_of_trust import RootOfTrust, SignedChange, SecurityError
 from .authorization import AuthorizationPolicy, authorize_change
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import authorize_release
+from .signing import SignedRelease, export_public_key_b64, sign_release, verify_release
 
 __all__ = [
     "AuthorizationPolicy",
     "RootOfTrust",
     "SecurityError",
     "SignedChange",
+    "SignedRelease",
     "authorize_change",
     "authorize_release",
     "build_manifest",
     "canonical_manifest",
+    "export_public_key_b64",
+    "sign_release",
+    "verify_release",
 ]
