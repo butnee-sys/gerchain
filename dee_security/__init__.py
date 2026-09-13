0e9773aef0e6969f084cf87ecca755f52ad31c34
butnee-sys <butnee@gmail.com>
2026-09-14 04:34:04 +0800

    feat: export DEE contract governance primitives

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -3,6 +3,7 @@
 from .root_of_trust import RootOfTrust, SignedChange, SecurityError
 from .authorization import AuthorizationPolicy, authorize_change
 from .identity import ProtectedIdentity, authorize_protected_operation
+from .contract_governance import ContractGovernanceError, ProtectedContract, authorize_contract_change
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
@@ -24,6 +25,7 @@ from .trinity import TrinityDecision, TrinityDimension, TrinityError, evaluate_t
 __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
     "authorize_change", "ProtectedIdentity", "authorize_protected_operation",
+    "ContractGovernanceError", "ProtectedContract", "authorize_contract_change",
     "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
     "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
