95731dea1175bd88c18af209ee5bbd30beef6c61
butnee-sys <butnee@gmail.com>
2026-09-14 04:41:35 +0800

    feat: expose escrow Trinity execution gate

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -7,6 +7,7 @@ from .contract_governance import ContractGovernanceError, ProtectedContract, aut
 from .evidence_governance import EvidenceGovernanceError, ProtectedEvidence, canonical_evidence, evidence_hash, require_protected_evidence
 from .core_access import CoreAccessError, CoreAccessRequest, authorize_core_access
 from .exim_port_security import EXIMPortRequest, EXIMPortSecurityError, authorize_exim_port_request
+from .escrow_trinity import EscrowExecutionProof, EscrowTrinityError, require_escrow_trinity
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
@@ -32,6 +33,7 @@ __all__ = [
     "EvidenceGovernanceError", "ProtectedEvidence", "canonical_evidence", "evidence_hash", "require_protected_evidence",
     "CoreAccessError", "CoreAccessRequest", "authorize_core_access",
     "EXIMPortRequest", "EXIMPortSecurityError", "authorize_exim_port_request",
+    "EscrowExecutionProof", "EscrowTrinityError", "require_escrow_trinity",
     "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
     "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
