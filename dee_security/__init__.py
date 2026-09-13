09faf18ec3f82d964d4bac14c181d13ba20a7fb3
butnee-sys <butnee@gmail.com>
2026-09-13 22:48:52 +0800

    feat: export DEE runtime governance

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -8,6 +8,14 @@ from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_
 from .signing import SignedRelease, sign_change, sign_release, verify_release
 from .rotation import KeyRegistry, KeyRotationRequest, build_rotation_request
 from .persistent_registry import PersistentKeyRegistry
+from .runtime_governance import (
+    RuntimeAction,
+    RuntimeAuthorization,
+    RuntimeGovernance,
+    RuntimeGovernanceError,
+    RuntimeIdentity,
+    RuntimeRole,
+)
 
 __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
@@ -15,4 +23,5 @@ __all__ = [
     "ReleaseAuthorization", "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
     "KeyRegistry", "KeyRotationRequest", "build_rotation_request", "PersistentKeyRegistry",
+    "RuntimeAction", "RuntimeAuthorization", "RuntimeGovernance", "RuntimeGovernanceError", "RuntimeIdentity", "RuntimeRole",
 ]
