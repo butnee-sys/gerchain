356d6a3651727c509862be0c773d1c02b8eaf220
butnee-sys <butnee@gmail.com>
2026-09-13 21:27:47 +0800

    feat(dee): export release authorization gate

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -3,7 +3,7 @@
 from .root_of_trust import RootOfTrust, SignedChange, SecurityError
 from .authorization import AuthorizationPolicy, authorize_change
 from .manifest import build_manifest, canonical_manifest
-from .release_policy import authorize_release
+from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
 from .signing import SignedRelease, sign_change, sign_release, verify_release
 from .rotation import KeyRegistry, KeyRotationRequest, build_rotation_request
@@ -11,7 +11,7 @@ from .rotation import KeyRegistry, KeyRotationRequest, build_rotation_request
 __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
     "authorize_change", "authorize_release", "build_manifest", "canonical_manifest",
-    "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
+    "ReleaseAuthorization", "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
     "KeyRegistry", "KeyRotationRequest", "build_rotation_request",
 ]
