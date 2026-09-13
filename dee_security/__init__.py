254e2f8f40e03f94a050a7f97164c51f62335b4a
butnee-sys <butnee@gmail.com>
2026-09-14 04:56:04 +0800

    Export DEE gateway governance

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -10,6 +10,7 @@ from .exim_port_security import EXIMPortRequest, EXIMPortSecurityError, authoriz
 from .escrow_trinity import EscrowExecutionProof, EscrowTrinityError, require_escrow_trinity
 from .witness_verification import WitnessVerificationError, WitnessVerificationProof, require_witness_verification, verify_witness_bundle
 from .connector_governance import ConnectorAccessRequest, ConnectorGovernanceError, authorize_connector_access
+from .gateway_governance import GatewayAccessRequest, GatewayGovernanceError, authorize_gateway_access
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
@@ -28,7 +29,7 @@ __all__ = [
     "CoreAccessError", "CoreAccessRequest", "authorize_core_access", "EXIMPortRequest", "EXIMPortSecurityError", "authorize_exim_port_request",
     "EscrowExecutionProof", "EscrowTrinityError", "require_escrow_trinity", "WitnessVerificationError", "WitnessVerificationProof",
     "require_witness_verification", "verify_witness_bundle", "ConnectorAccessRequest", "ConnectorGovernanceError", "authorize_connector_access",
-    "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
+    "GatewayAccessRequest", "GatewayGovernanceError", "authorize_gateway_access", "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
     "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64", "SignedRelease", "sign_change", "sign_release", "verify_release",
     "KeyRegistry", "KeyRotationRequest", "build_rotation_request", "PersistentKeyRegistry", "RuntimeAction", "RuntimeAuthorization", "RuntimeGovernance", "RuntimeGovernanceError", "RuntimeIdentity", "RuntimeRole",
     "RecoveryApproval", "RecoveryAuthority", "RecoveryDecision", "RecoveryGovernance", "RecoveryGovernanceError", "RecoveryPolicy", "RecoveryRequest", "RecoveryRole", "build_recovery_approval",
