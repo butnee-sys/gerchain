0857d72b8404f065f9e8576591e7cc0de55aaea7
butnee-sys <butnee@gmail.com>
2026-09-14 04:31:52 +0800

    feat: export DEE protected identity boundary

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -2,6 +2,7 @@
 
 from .root_of_trust import RootOfTrust, SignedChange, SecurityError
 from .authorization import AuthorizationPolicy, authorize_change
+from .identity import ProtectedIdentity, authorize_protected_operation
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
@@ -9,22 +10,12 @@ from .signing import SignedRelease, sign_change, sign_release, verify_release
 from .rotation import KeyRegistry, KeyRotationRequest, build_rotation_request
 from .persistent_registry import PersistentKeyRegistry
 from .runtime_governance import (
-    RuntimeAction,
-    RuntimeAuthorization,
-    RuntimeGovernance,
-    RuntimeGovernanceError,
-    RuntimeIdentity,
-    RuntimeRole,
+    RuntimeAction, RuntimeAuthorization, RuntimeGovernance, RuntimeGovernanceError,
+    RuntimeIdentity, RuntimeRole,
 )
 from .recovery import (
-    RecoveryApproval,
-    RecoveryAuthority,
-    RecoveryDecision,
-    RecoveryGovernance,
-    RecoveryGovernanceError,
-    RecoveryPolicy,
-    RecoveryRequest,
-    RecoveryRole,
+    RecoveryApproval, RecoveryAuthority, RecoveryDecision, RecoveryGovernance,
+    RecoveryGovernanceError, RecoveryPolicy, RecoveryRequest, RecoveryRole,
     build_recovery_approval,
 )
 from .genesis import GenesisAnchor, GenesisError, build_genesis_anchor, verify_genesis_anchor
@@ -32,13 +23,14 @@ from .trinity import TrinityDecision, TrinityDimension, TrinityError, evaluate_t
 
 __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
-    "authorize_change", "authorize_release", "build_manifest", "canonical_manifest",
-    "ReleaseAuthorization", "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
+    "authorize_change", "ProtectedIdentity", "authorize_protected_operation",
+    "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
+    "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
     "KeyRegistry", "KeyRotationRequest", "build_rotation_request", "PersistentKeyRegistry",
-    "RuntimeAction", "RuntimeAuthorization", "RuntimeGovernance", "RuntimeGovernanceError", "RuntimeIdentity", "RuntimeRole",
-    "RecoveryApproval", "RecoveryAuthority", "RecoveryDecision", "RecoveryGovernance", "RecoveryGovernanceError",
-    "RecoveryPolicy", "RecoveryRequest", "RecoveryRole", "build_recovery_approval",
-    "GenesisAnchor", "GenesisError", "build_genesis_anchor", "verify_genesis_anchor",
+    "RuntimeAction", "RuntimeAuthorization", "RuntimeGovernance", "RuntimeGovernanceError",
+    "RuntimeIdentity", "RuntimeRole", "RecoveryApproval", "RecoveryAuthority", "RecoveryDecision",
+    "RecoveryGovernance", "RecoveryGovernanceError", "RecoveryPolicy", "RecoveryRequest", "RecoveryRole",
+    "build_recovery_approval", "GenesisAnchor", "GenesisError", "build_genesis_anchor", "verify_genesis_anchor",
     "TrinityDecision", "TrinityDimension", "TrinityError", "evaluate_trinity", "require_trinity",
 ]
