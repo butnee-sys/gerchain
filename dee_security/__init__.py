4f42fb09b696a145883cfd81df750d57047df729
butnee-sys <butnee@gmail.com>
2026-09-13 21:37:53 +0800

    feat: export persistent DEE owner registry

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -7,11 +7,12 @@ from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
 from .signing import SignedRelease, sign_change, sign_release, verify_release
 from .rotation import KeyRegistry, KeyRotationRequest, build_rotation_request
+from .persistent_registry import PersistentKeyRegistry
 
 __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
     "authorize_change", "authorize_release", "build_manifest", "canonical_manifest",
     "ReleaseAuthorization", "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
-    "KeyRegistry", "KeyRotationRequest", "build_rotation_request",
+    "KeyRegistry", "KeyRotationRequest", "build_rotation_request", "PersistentKeyRegistry",
 ]
