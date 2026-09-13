591fc394547785b0b18297e071fefb8715e29f05
butnee-sys <butnee@gmail.com>
2026-09-14 04:35:37 +0800

    feat: expose DEE evidence governance primitives

--- dee_security/__init__.py
+++ dee_security/__init__.py
@@ -4,6 +4,7 @@ from .root_of_trust import RootOfTrust, SignedChange, SecurityError
 from .authorization import AuthorizationPolicy, authorize_change
 from .identity import ProtectedIdentity, authorize_protected_operation
 from .contract_governance import ContractGovernanceError, ProtectedContract, authorize_contract_change
+from .evidence_governance import EvidenceGovernanceError, ProtectedEvidence, canonical_evidence, evidence_hash, require_protected_evidence
 from .manifest import build_manifest, canonical_manifest
 from .release_policy import ReleaseAuthorization, authorize_release
 from .key_management import OwnerKeyRecord, generate_owner_keypair, key_id_from_public_key, load_private_key, public_key_b64
@@ -26,6 +27,7 @@ __all__ = [
     "AuthorizationPolicy", "RootOfTrust", "SecurityError", "SignedChange",
     "authorize_change", "ProtectedIdentity", "authorize_protected_operation",
     "ContractGovernanceError", "ProtectedContract", "authorize_contract_change",
+    "EvidenceGovernanceError", "ProtectedEvidence", "canonical_evidence", "evidence_hash", "require_protected_evidence",
     "authorize_release", "build_manifest", "canonical_manifest", "ReleaseAuthorization",
     "OwnerKeyRecord", "generate_owner_keypair", "key_id_from_public_key", "load_private_key", "public_key_b64",
     "SignedRelease", "sign_change", "sign_release", "verify_release",
