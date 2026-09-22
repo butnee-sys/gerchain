b0928e14091425ec0359eb0351d2c236cd772f24
butnee-sys <butnee@gmail.com>
2026-09-22 22:23:00 +0800

    chore(production): remove unused entrypoint session factory

--- production_entrypoint.py
+++ production_entrypoint.py
@@ -5,7 +5,6 @@ import signal
 import time
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 from services.gerchain_runtime_factory import ProductionRuntimeFactory
 
@@ -42,8 +41,6 @@ def main() -> None:
         raise RuntimeError("GERCHAIN_ESCROW_AMOUNT must be an integer") from exc
 
     engine = create_engine(database_url, pool_pre_ping=True)
-    session_factory = sessionmaker(bind=engine, expire_on_commit=False)
-
     try:
         runtime = ProductionRuntimeFactory.create(
             escrow_id=escrow_id,
