2fea8a3e1eb84c11fc77b380bbb8ee55fb69ee3a
butnee-sys <butnee@gmail.com>
2026-09-27 00:28:36 +0800

    chore(production): remove duplicate schema guard import

--- services/gerchain_runtime_factory.py
+++ services/gerchain_runtime_factory.py
@@ -10,7 +10,6 @@ from sqlalchemy.orm import sessionmaker
 
 from postgres.migrations import apply_migrations
 from persistence.production_schema_guard import assert_canonical_production_schema
-from persistence.production_schema_guard import assert_canonical_production_schema
 from services.gerchain_runtime import GerchainRuntime
 
 
